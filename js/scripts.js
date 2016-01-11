8dc127332df51a2cb63698ca10f078a0494e7eb3
Scott Holland <scotthollandjr@gmail.com>
2016-01-10 23:14:19 -0800

    Final edit with pictures

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,43 @@
-scripts.js
+var a = 0;
+var e = 0;
+var w = 0;
+var f = 0;
+
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    var quests = ["quest1", "quest2", "quest3", "quest4", "quest5"];
+
+    quests.forEach(function(quest) {
+      var answer = $("input[name=" + quest + "]:checked").val();
+
+      if (answer === "a") {
+        a ++;
+      } else if (answer === "e") {
+        e ++;
+      } else if (answer === "w") {
+        w ++;
+      } else if (answer === "f") {
+        f ++;
+      }
+
+    });
+
+    if (a >= 3) {
+    $("#survey").toggle();
+    $("#air").toggle();
+  } else if (e >= 3) {
+    $("#survey").toggle();
+    $("#earth").toggle();
+  } else if (w >= 3) {
+    $("#survey").toggle();
+    $("#water").toggle();
+  } else if (f >= 3) {
+    $("#survey").toggle();
+    $("#fire").toggle();
+  } else {
+    $("#survey").toggle();
+    alert("Sorry, bruh. You ain't no bender!")
+  }
+    event.preventDefault();
+  });
+});
